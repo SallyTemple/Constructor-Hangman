068fc6ecf255cf5f9e65405a928a4ced2837def1
SallyTemple <pfende@gmail.com>
2018-04-15 20:03:33 -0700

    update

--- letter.js
+++ letter.js
@@ -1,3 +1,19 @@
 
 var is_letter_a = require('is_letter_a');
+// constructor function which can be used to create "letter" objects
+var Letter = function() {
+      this.value = "";
+      this.guessed = false;
+}
 
+//method returns the underlying character if the letter has been guessed
+this.guessedLetter = function() {
+      if (this.guessed) {
+        console.log(true);
+        
+      }
+      else {
+        console.log(false);
+      }
+    };
+  
--- word.js
+++ word.js
@@ -6,12 +6,7 @@ function WordBee(value) {
   this.value = value
   this.letter = [];
   this.guesses = "";
-  
-  // creates the printInfo method and applies it to all programmer objects
-  this.printInfo = function() {
-    console.log("Name: " + this.name + "\nPosition: " + this.position +
-    "\nAge: " + this.age + "\nLanguages: " + this.language);
   };
-}
+
 
 module.exports = WordBee;
